d6b22d25218349270b29448486beea3726fdedb6
Milkweedd <antontab91@gmail.com>
2020-09-22 17:12:12 +0300

    commit

--- src/UserProfile.jsx
+++ src/UserProfile.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { useHistory, useLocation, useParams, useRouteMatch } from 'react-router-dom';
 
 const UserProfile = (props) => {
   console.log(props)
+  console.log(useParams())
+  console.log(useHistory())
+  console.log(useLocation())
+  console.log(useRouteMatch())
+
   return (
     <div>
       <h1>{`User Profile: ${props.match.params.userId}`}</h1>
